918b13121ac9c3ffbc3c192e1f568435a2a8c525
jonathan lima ferreira <jonathan-jonas04@hotmail.com>
2019-03-22 02:12:16 -0300

    meu primeiro programa

---
+++ Assets/Scripts/GameMainController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class GameMainController : MonoBehaviour
+{
+    public Text scoreText;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello");
+        scoreText.text = "Score " + score;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
